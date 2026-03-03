bfc7a5f1e5963cce61c3a009ce0ceb9a9472072f
Muthu Rajesh T <muthurajesht041@gmail.com>
2026-03-04 04:57:05 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Shield, Bell, History, Camera, AlertTriangle } from 'lucide-react';
 import './App.css';
 
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:3000";
 
 
 
